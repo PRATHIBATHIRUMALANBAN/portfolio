27577f455cc9315bf89fae073967ac7c5bc6f297
PRATHIBATHIRUMALANBAN <prathiba.tmuthu@gmail.com>
2024-10-05 22:42:48 +0530

    Add files via upload

--- portfolio.py
+++ portfolio.py
@@ -52,22 +52,6 @@ def projects():
     - Description: [From this project you can be able to find out the visualisation of risk management for specified
     And also it works As search bar to find the posting of an accounting item.]
     """)
-from PIL import Image
-
-
-
-
-# Multiple file uploader
-uploaded_files = st.file_uploader("screenshoot1.py.png", accept_multiple_files=True, type=["png", "jpg", "jpeg"])
-
-# Display the uploaded images
-if uploaded_files:
-    st.write(f"Uploaded {len(uploaded_files)} image(s):")
-    for uploaded_file in uploaded_files:
-        # Open the image file
-        image = Image.open(uploaded_file)
-        # Display image
-        st.image(image, caption=uploaded_file.name, use_column_width=True)
     
 
 
@@ -88,3 +72,18 @@ elif options == "Projects":
     projects()
 elif options == "Contact":
     contact()
+from PIL import Image
+
+
+# Multiple file uploader
+uploaded_files = st.file_uploader("screenshoot1.py.png", accept_multiple_files=True, type=["png", "jpg", "jpeg"])
+
+# Display the uploaded images
+if uploaded_files:
+    st.write(f"Uploaded {len(uploaded_files)} image(s):")
+    for uploaded_file in uploaded_files:
+        # Open the image file
+        image = Image.open(uploaded_file)
+        # Display image
+        st.image(image, caption=uploaded_file.name, use_column_width=True)
+    
